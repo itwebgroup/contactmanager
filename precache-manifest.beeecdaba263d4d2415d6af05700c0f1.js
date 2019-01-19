3e45523210e6ef1528cee731da1ba8cb1ef09755
Riccardo Sacco <rickybag99@MBP-di-Riccardo.station>
2019-01-19 01:35:38 +0100

    Updates

---
+++ precache-manifest.beeecdaba263d4d2415d6af05700c0f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "c916d0dff7895d4eb547",
+    "url": "/contactmanager/static/js/main.c916d0df.chunk.js"
+  },
+  {
+    "revision": "972f6d772c5c42a073ab",
+    "url": "/contactmanager/static/js/1.972f6d77.chunk.js"
+  },
+  {
+    "revision": "c916d0dff7895d4eb547",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "972f6d772c5c42a073ab",
+    "url": "/contactmanager/static/css/1.bb871d76.chunk.css"
+  },
+  {
+    "revision": "24e697524f08d269f5c05707cc4e8aaf",
+    "url": "/contactmanager/index.html"
+  }
+];
