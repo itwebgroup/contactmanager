e06ab7a4c715233ab438699bbe2ff6c2ed731c56
Riccardo Sacco <rickybag99@MBP-di-Riccardo.station>
2019-01-19 01:32:38 +0100

    inital commit

---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import axios from "axios";
+
+import { Consumer } from "../../context";
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onShowClick = () => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
+
+  onDeleteClick = async (id, dispatch) => {
+    try {
+      await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+      dispatch({ type: "DELETE_CONTACT", payload: id });
+    } catch (e) {
+      dispatch({ type: "DELETE_CONTACT", payload: id });
+    }
+  };
+
+  render() {
+    const { id, name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4 className="mb-0">
+                {name}{" "}
+                <i
+                  onClick={this.onShowClick}
+                  className="fas fa-sort-down"
+                  style={{ cursor: "pointer" }}
+                />
+                <i
+                  className="fas fa-times"
+                  style={{
+                    cursor: "pointer",
+                    float: "right",
+                    color: "red",
+                    fontSize: 28
+                  }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+                <Link to={`contact/edit/${id}`}>
+                  <i
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: "pointer",
+                      float: "right",
+                      marginRight: "1rem"
+                    }}
+                  />
+                </Link>
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group mt-2">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default Contact;
